5049e8ba65d93301e6e0caf4681610657d2218f8
preslavsh <p.hadzhitsanev@nolimit.studio>
2017-12-18 19:00:49 +0200

    update loops

--- 4-loops/4-zad-factoriel.cpp
+++ 4-loops/4-zad-factoriel.cpp
@@ -4,11 +4,24 @@
 using namespace std;
 
 int main() {
-    int number, fact = 1;
-    cin >> number;
-    for(int i = number; i > 0; i--){
-        fact = fact * i;
+    int dec_number = 51;
+    int left = 0;
+    while(dec_number){
+        if(dec_number < 16 ) left = dec_number;
+        else left = dec_number%16;
+
+        if(left < 10) cout << left;
+        else {
+            switch(left){
+                case 10: cout << "A" << endl;
+                case 11: cout << "B" << endl;
+                case 12: cout << "C" << endl;
+                case 13: cout << "D" << endl;
+                case 14: cout << "E" << endl;
+                case 15: cout << "F" << endl;
+            }
+        }
+        dec_number/=16;
     }
-    cout << fact << endl;
     return 0;
 }
